d97721dc4a188f4c681b3d620cee43a7707ed9a3
Loïc Goyet <loic.goyet@gmail.com>
2017-11-11 18:09:13 +0100

    Save state into localstorage
    Allows the user to reload the page without loosing its session

--- src/store.js
+++ src/store.js
@@ -1,14 +1,23 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import throttle from 'lodash/throttle'
 
 import rootReducer from './reducers'
 import middlewares from './actions/middlewares'
+import { loadState, saveState } from './localStorage'
+
+const persistedState = loadState();
 
 const store = createStore(
   rootReducer,
+  persistedState,
   composeWithDevTools(
     applyMiddleware(...middlewares)
   )
 )
+
+store.subscribe(throttle(() => {
+  saveState(store.getState())
+}), 1000)
 
 export default store
