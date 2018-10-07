3c8d5ce1c40467a0448a99c738b49710ded24d22
LoicGoyet <loic.goyet@gmail.com>
2018-10-07 15:20:29 +0200

    create component <Screen>

---
+++ src/components/Screen/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.scss';
+
+const Screen = () => (
+  <div
+    className="screen"
+    style={{
+      backgroundImage:
+        'url("https://raw.githubusercontent.com/LoicGoyet/The-Folio-Of-Which-You-Are-The-Hero/master/src/components/rooms/entrance/illustration.gif")',
+    }}
+  />
+);
+
+export default Screen;
---
+++ src/components/Screen/stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Screen from '.';
+
+const stories = storiesOf('Screen', module);
+
+stories.add('default', () => <Screen />);
+
+export default stories;
